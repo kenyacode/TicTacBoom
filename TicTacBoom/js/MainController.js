040a41729a0c2d61aa5295c3c55dc1d3f148a663
Joshua Munene <joshuamwangi@gmail.com>
2014-12-09 21:54:37 -0800

    adding angular, main controller, and app.js files

---
+++ TicTacBoom/js/MainController.js
@@ -0,0 +1,9 @@
+//between the view and model
+//2nd new page created and saved.
+//Step 3.1
+
+(function(){
+
+
+
+}());
---
+++ TicTacBoom/js/app.js
@@ -0,0 +1,9 @@
+//IIFE
+// 1st page created and saved
+// Step 2.1
+
+(function(){
+
+	angular.module("app", []);
+
+}())
