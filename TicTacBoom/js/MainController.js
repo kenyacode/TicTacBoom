9a5e5bedbcab142112ee2ab26e1e03e04f6937a3
Joshua Munene <joshuamwangi@gmail.com>
2014-12-10 20:55:27 -0800

    added rows and cells in rows for the gameboard

--- TicTacBoom/js/MainController.js
+++ TicTacBoom/js/MainController.js
@@ -5,16 +5,12 @@ boomAppCtlr = function($scope){
 		//this makes the gameboard
 		var game = {};
 		game.board = [
-			{ box : 1, owner : " "},
-			{ box : 2, owner : " "},
-			{ box : 3, owner : " "},
-			{ box : 4, owner : " "},
-			{ box : 5, owner : " "},
-			{ box : 6, owner : " "},
-			{ box : 7, owner : " "},
-			{ box : 8, owner : " "},
-			{ box : 9, owner : " "},
-		];
+			[{s:"X"},{s:" "},{s:" "}],
+			[{s:"X"},{s:"O"},{s:" "}],
+			[{s:"X"},{s:" "},{s:"X"}],
+			// ["x","",""],
+			// ["Y","Y","Y"]
+			];
 $scope.game = game;
 	};
 
