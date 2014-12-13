8ee00de29eddf2f70e94e35b16378afe0b1deacb
Joshua Munene <joshuamwangi@gmail.com>
2014-12-12 17:13:06 -0800

    added divs to my view to call in my controller and to manipulate in the css.

--- TicTacBoom/js/MainController.js
+++ TicTacBoom/js/MainController.js
@@ -1,22 +1,68 @@
 //between the view and model
 //2nd new page created and saved.
 //Step 3.1
+
+
 boomAppCtlr = function($scope){
 		//this makes the gameboard
 		var game = {};
+		var player1 = new Player();
+		var computer = new Computer();
+		var currentPlayer = player1;
 		game.board = [
 
-				{s:"X"},{s:"X"},{s:"O"},
-				{s:"X"},{s:"O"},{s:"X"},
-				{s:"X"},{s:"O"},{s:"X"}
+				{s:""},{s:""},{s:""},
+				{s:""},{s:""},{s:""},
+				{s:""},{s:""},{s:""}
 
 			];
-$scope.game = game;
+	$scope.cellClick = function() {
+		
+		function cellClick() {
+			changeColor();
+		}
+
+		cellClick();
 	};
+
+	$scope.game = game;
+	
+	function changeColor() {
+			if (player1) {
+				return ".selected-x"
+			}
+			if (computer)	{
+				return ".selected-o"
+			}
+		switchPlayer();
+	};
+
+	function switchPlayer(){
+      if (currentPlayer == player1){
+        currentPlayer = computer;
+      }else{
+        currentPlayer = player1;
+      }
+      	// console.log(switchPlayer());
+	};
+
+	$scope.fillBox = function(obj){
+      var dataValue = obj.srcElement.attributes.data.value;
+      var boxToChange = document.getElementById(dataValue);
+      boxToChange.style.backgroundColor = currentPlayer.color
+
+	}
+      currentPlayer = new Player();
+      console.log(currentPlayer)
+      switchPlayer();
+
+};
 
 angular
 	.module("boomApp", [])
 	.controller("boomAppCtlr", boomAppCtlr);
 
-// Things do not change
-// 2 Players, 3x3 Gameboard, 
+
+    
+
+  
---
+++ TicTacBoom/js/playerCtlr.js
@@ -0,0 +1,10 @@
+function Player(){
+	this.name = "";
+	this.img = "./images/ttt_x.png"
+
+}
+
+function Computer(){
+	this.name = " "
+	this.img = "./images/ttt_y.png"
+}
